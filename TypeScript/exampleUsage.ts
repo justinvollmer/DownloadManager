8f421734da8941c464ea2ae485f257334cde014e
Justin Vollmer <justinvollmer01@googlemail.com>
2023-09-15 20:40:24 +0200

    Update: TypeScript example usage

--- TypeScript/exampleUsage.ts
+++ TypeScript/exampleUsage.ts
@@ -1,16 +1,20 @@
 import { download } from "./DownloadManager";
 
-try {
-  const imageUrl = 'https://example.com/imagefile.jpg';
-  const videoUrl = 'https://example.com/videofile';
-  const customFileType = 'png';
-  const outputFolder = './downloads'; // Specify the output folder
+async function downloadFiles() {
+  try {
+    const imageUrl = 'https://example.com/imagefile.jpg';
+    const videoUrl = 'https://example.com/videofile.mp4';
+    const customFileType = 'mkv';
+    const outputFolder = './'; // Specify the output folder
 
-  await download(imageUrl, 'downloaded_image', outputFolder);
-  console.log('Image downloaded successfully.');
+    await download(imageUrl, 'downloaded_image', outputFolder);
+    console.log('Image downloaded successfully.');
 
-  await download(videoUrl, 'downloaded_video', outputFolder, customFileType);
-  console.log('Video downloaded successfully.');
-} catch (error) {
-  console.error(`Error: ${error instanceof Error ? error.message : 'Unknown error'}`);
+    await download(videoUrl, 'downloaded_video', outputFolder, customFileType);
+    console.log('Video downloaded successfully.');
+  } catch (error) {
+    console.error(`Error: ${error instanceof Error ? error.message : 'Unknown error'}`);
+  }
 }
+
+downloadFiles();
